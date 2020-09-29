7ae7c46204db4c3a54c3de50f6d8c4659087adf7
YuliaPol <julia.polyeshko@gmail.com>
2020-09-29 13:00:55 +0300

    add more type of question

--- js/script.js
+++ js/script.js
@@ -37,6 +37,55 @@ jQuery(function ($) {
             }
         });
 
+        //ranging
+        //ranging up 
+        $('.container').on('click', '.ranging-list .item-up', function(e){
+            if($(this).parents('.rangint-item').prev().length>0){
+                $(this).parents('.rangint-item').insertBefore($(this).parents('.rangint-item').prev());
+            }
+        });
+
+        //ranging down 
+        $('.container').on('click', '.ranging-list .item-down', function(e){
+            if($(this).parents('.rangint-item').next().length>0){
+                $(this).parents('.rangint-item').insertAfter($(this).parents('.rangint-item').next());
+            }
+        });
+        //ranging sortable
+        $('.container .ranging-list').sortable({});
+
+        //phone cpuntry code 
+        $('.phone-wrapper input.code').intlTelInput({
+            initialCountry: "ru",
+        });
+
+        //date russian calendar
+        $.datepicker.setDefaults(
+            {
+            closeText: 'Закрыть',
+            prevText: '',
+            currentText: 'Сегодня',
+            monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+                'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+            monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+                'Июл','Авг','Сен','Окт','Ноя','Дек'],
+            dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+            dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+            dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+            weekHeader: 'Не',
+            dateFormat: 'dd.mm.yy',
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: ''
+        });
+        //set plugins for date
+        $('.date-wrapper input').datepicker();
+        //show calendar
+        $('.container').on('click', '.date-wrapper .icon-date', function(e){
+            $(this).parents('.date-wrapper').find('input').datepicker('show');
+        });
+
         //validation
         var formValid = document.getElementsByClassName('form-valid')[0];
         $('.valid-form-send').click(function () {
@@ -48,9 +97,10 @@ jQuery(function ($) {
                     if (el[i].value === '' || el[i].value === ' ' || el[i].value === '-') {
                         erroreArrayElemnts.push(el[i]);
                         $('.modal').fadeIn(300);
-                        $(el[i]).addClass('has-error');
+                        $('.modal').find('.text').html("Введите, пожалуйста, ответ.");
+                        $(el[i]).parents('.question-wrapper').addClass('has-error');
                         $(el[i]).focus(function (e) {
-                            $(e.target).removeClass('has-error');
+                            $(e.target).parents('.question-wrapper').removeClass('has-error');
                         });
                     }
                 }
@@ -64,6 +114,10 @@ jQuery(function ($) {
                             if($(el[i]).parents('.question-wrapper')){
                                 $(el[i]).parents('.question-wrapper').addClass('has-error');
                             }
+                            $(el[i]).focus(function (e) {
+                                $(e.target).parents('.question-wrapper').removeClass('has-error');
+                            });    
+                            $('.modal').find('.text').html("Выберете, пожалуйста, ответ.");
                             $('.modal').fadeIn(300);
                         }
                     }
