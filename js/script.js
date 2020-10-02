9d188c4246f45e0a797589dfab90cbd10f993ea9
YuliaPol <julia.polyeshko@gmail.com>
2020-10-02 12:13:49 +0300

    Add cursor:pointer, less size for imagw and audio

--- js/script.js
+++ js/script.js
@@ -51,6 +51,30 @@ jQuery(function ($) {
                 $(this).parents('.rangint-item').insertAfter($(this).parents('.rangint-item').next());
             }
         });
+
+        //branching hidden question show
+        $('.container').on('change', '.branching-wrapper input[type=radio]', function(e){
+            var idQuestion = $(this).attr('id').split('-')[1];
+            var idPoint = parseInt($(this).attr('id').split('-')[2]);
+            if($(this).is(':checked')){
+                $(this).parents('.question-wrapper').find('.hidden-question-group').fadeOut(300);
+                $('#hiddenquestion_' + idQuestion + '_' + idPoint).fadeIn(300);
+            }
+        });
+
+        //dropdown show\hide
+        $('.container').on('click', '.dropdown-list .question-name', function(e){
+            if($(this).parents('.dropdown-block').hasClass('active')){
+                $(this).parents('.dropdown-block').removeClass('active');
+                $(this).parents('.dropdown-block').find('.dropdown-content').fadeOut(300);
+            }
+            else {
+                $(this).parents('.dropdown-list').find('.dropdown-block').removeClass('active');
+                $(this).parents('.dropdown-list').find('.dropdown-content').fadeOut(300);
+                $(this).parents('.dropdown-block').addClass('active');
+                $(this).parents('.dropdown-block').find('.dropdown-content').fadeIn(300);
+            }
+        });
         //ranging sortable
         $('.container .ranging-list').sortable({});
 
@@ -59,6 +83,50 @@ jQuery(function ($) {
             initialCountry: "ru",
         });
 
+        $('.audiowave').each(function(){
+
+            //Generate unic ud
+            var path = $(this).attr('data-audiopath');//path for audio
+            var id = '_' + Math.random().toString(36).substr(2, 9);
+
+            //Set id to container
+            $(this).attr('id', id);
+        
+            //Initialize WaveSurfer
+            var wavesurfer = WaveSurfer.create({
+                container: '#' + id,
+                scrollParent: true,
+                backgroundColor: '#FFFFFF',
+                height: 40,
+                barMinHeight: 1,
+                barWidth: 1.5,
+                cursorWidth: 0,
+                barGap: 2,
+                waveColor: 'rgba(87, 34, 222, 0.2)',
+                hideScrollbar: true,
+                progressColor: "#5722DE"
+            });
+        
+            //Load audio file
+            wavesurfer.load(path);
+        
+            //Add button event
+            $(this).parents('.audioblock').find('.playaudio').click(function(){
+                wavesurfer.playPause();
+            });
+        });
+        
+        //video play
+        $('.container').on('click', '.videoblock', function(e){
+            if($(this).children('video').get(0).paused){
+                $(this).children('video').get(0).play();
+                $(this).children('.play').fadeOut();
+            }else{
+                $(this).children('video').get(0).pause();
+                $(this).children('.play').fadeIn();
+            }
+        });
+        
         //date russian calendar
         $.datepicker.setDefaults(
             {
